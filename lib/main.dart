ffb81cfcb2464bb9ed156bcea562f296997994dc
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-05-07 14:41:51 -0300

    create homepage app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Placeholder());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'StateNotifier',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyhomePage(),
+    );
+  }
+}
+
+class MyhomePage extends StatelessWidget {
+  const MyhomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text('StateNotifier'),
+      ),
+      body: Center(
+        child: Text(
+          '0',
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            tooltip: 'Increment',
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 10.0),
+          FloatingActionButton(
+            tooltip: 'Change Color',
+            onPressed: () {},
+            child: const Icon(Icons.color_lens_outlined),
+          ),
+        ],
+      ),
+    );
+  }
 }
